396a04ef13b83a5900a2896001f961b5786cb138
Ryan Brenner <ryanbrenner@Ryans-MacBook-Air-366.local>
2026-03-23 16:13:44 -0400

    Began work

--- project2_starter.py
+++ project2_starter.py
@@ -1,16 +1,11 @@
 # SI 201 HW4 (Library Checkout System)
-# Your name:
-# Your student id:
-# Your email:
+# Your name: Ryan Brenner
+# Your student id: ryanbren
+# Your email: ryanbren@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
 # If you worked with generative AI also add a statement for how you used it.
-# e.g.:
-# Asked ChatGPT for hints on debugging and for suggestions on overall code structure
-#
-# Did your use of GenAI on this assignment align with your goals and guidelines in your Gen AI contract? If not, why?
-#
-# --- ARGUMENTS & EXPECTED RETURN VALUES PROVIDED --- #
-# --- SEE INSTRUCTIONS FOR FULL DETAILS ON METHOD IMPLEMENTATION --- #
+# I used AI to help me debug and to better understand Soup/XML. Since I missed a lecture, I needed to use 
+# ChatGPT to help me catch up and learn as I did the project. 
 
 from bs4 import BeautifulSoup
 import re
